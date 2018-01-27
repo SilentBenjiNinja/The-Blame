3c3dcc29eeb0017c35a877b369a41235947e819d
Benji-Laptop\Zombie <wisehobo@dahood.net>
2018-01-27 14:16:33 +0100

    Some gameplay scripts

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour {
+
+    public bool actionDoneThisRound;
+    public GameObject manager;
+
+
+    private void Start()
+    {
+        actionDoneThisRound = false;
+    }
+
+    public void newRound()
+    {
+        actionDoneThisRound = false;
+    }
+    
+    public void shiftWorkToPlayer(int playerId)
+    {
+
+        actionDoneThisRound = true;
+    }
+}
