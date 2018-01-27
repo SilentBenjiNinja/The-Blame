e0a8cafeb600fa18ff49f6c9247ba16ad4ef2732
DESKTOP-J5N34VU\Basti <s.gsaenger@gmx.de>
2018-01-27 17:09:52 +0100

    Adding Startmenu

---
+++ Assets/Photon Unity Networking/Demos/PunBasics-Tutorial/Scripts/StartMenuManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour {
+
+    private Button hostGame;
+    private Button joinGame;
+    private Button howToPlay;
+    private Button backButton;
+    private Button endGame;
+
+    private void Start()
+    {
+        hostGame = GameObject.Find("HostGame").GetComponent<Button>();
+        joinGame = GameObject.Find("JoinGame").GetComponent<Button>();
+        howToPlay = GameObject.Find("HowToPlay").GetComponent<Button>();
+        backButton = GameObject.Find("BackButton").GetComponent<Button>();
+        endGame = GameObject.Find("ExitGame").GetComponent<Button>();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        //if(hostGame.enabled == false)
+        //{
+        //    ActivateStartmenu();
+        //}
+
+        
+
+    }
+
+    private void ActivateStartmenu()
+    {
+        hostGame.enabled = false;
+        joinGame.enabled = false;
+        howToPlay.enabled = false;
+    }
+
+    private void DectivateStartmenu()
+    {
+        hostGame.enabled = false;
+        joinGame.enabled = false;
+        howToPlay.enabled = false;
+    }
+
+    public void OpenHowToPlay()
+    {
+        DectivateStartmenu();
+    }
+}
