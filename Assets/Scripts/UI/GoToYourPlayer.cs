1059cccf2283a7855eafdbde9b5b4adceb564803
Benji-Laptop\Zombie <wisehobo@dahood.net>
2018-01-28 11:21:26 +0100

    basically no changes---

---
+++ Assets/Scripts/UI/GoToYourPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToYourPlayer : MonoBehaviour {
+
+    public Transform transformButton;
+
+    void Start () {
+        this.transform.position = transformButton.position;
+	}
+}
